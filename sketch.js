c64de85ca3c2a2918362b8569918cf9b8bec5f4d
krisha-ops <71079030+krisha-ops@users.noreply.github.com>
2020-11-20 17:58:16 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+var canvas,backgroundImg;
+var gameState=0;
+var playerCount;
+var database;
+var form,player,game;
+
+function setup(){
+  createCanvas(400,400);
+  database = firebase.database();
+  console.log(database);
+  game = new Game();
+  game.getState();
+  game.start();
+  
+}
+
+function draw() {
+  
+}
+
